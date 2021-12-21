97a0e3358e1a8f67a52b9ce8e228da77800b889d
Eeshanbachu <89650307+Eeshanbachu@users.noreply.github.com>
2021-12-21 21:04:19 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,55 @@
+var canvas= document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+var color="blue";
+
+
+ctx .beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+var canvas= document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+var color="black";
+
+
+ctx .beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(285,200,40,0,2*Math.PI);
+ctx.stroke();
+
+var canvas= document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+var color="red";
+
+
+ctx .beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(370,200,40,0,2*Math.PI);
+ctx.stroke();
+
+var canvas= document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+var color="yellow";
+
+
+ctx .beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(242,250,40,0,2*Math.PI);
+ctx.stroke();
+
+var canvas= document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+var color="green";
+
+
+ctx .beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(327,250,40,0,2*Math.PI);
+ctx.stroke();
+
